c1f7ea3f9e191e166e66ee7a0fc973866729bda6
Jean Ponchon <jean.ponchon@novadiscovery.com>
2015-05-22 18:42:02 +0200

    feat: Add matrix reporter

--- demo/sandbox.test.js
+++ demo/sandbox.test.js
@@ -2,13 +2,14 @@ var expect = require('expect.js')
 var Promise = require('bluebird')
 
 describe('A client-server test suite', function() {
+  this.timeout(5000)
+
   before(function() {
     var div = document.createElement('div')
     div.id = 'foobar'
     div.style.position = 'absolute'
     div.style.top = '16px'
     div.style.left= '320px'
-    div.innerHTML = 'Hello'
     document.body.appendChild(div)
   })
 
@@ -22,6 +23,10 @@ describe('A client-server test suite', function() {
     var box = div.getClientRects()[0]
     expect(box.top).to.be(16)
     expect(box.left).to.be(320)
+  })
+
+  it('should be long too', function(done) {
+    setTimeout(done, 1200)
   })
 
 })
